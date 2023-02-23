198b9e50d723f0b1ec288ccdaa0d20675aae9632
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-23 13:27:30 +0100

    Update Lead model

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 
 class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        # The first element is how the choice gets stored in the db, the second element is the display value
+        ('YouTube', 'YouTube'),
+        ('Google', 'Google'),
+        ('Newsletter', 'Newsletter'),
+    )
+
     # Setting the max_length on CharFields is required
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
