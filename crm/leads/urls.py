b4b29589b27f68ba131dcb296012220f7fa40567
langBeralarm <114937969+langBeralarm@users.noreply.github.com>
2023-02-23 16:26:14 +0100

    Revert "Feature/add homepage view"

--- crm/leads/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import lead_list
-
-app_name = 'leads'
-
-urlpatterns = [
-    path('', lead_list, name='home_page'),
-]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from .models import Lead
 
-
-def lead_list(request):
-    leads = Lead.objects.all()
-    return render(request, 'leads/lead_list.html', {
-        'leads': leads,
-    })
+# Create your views here.
