f8dbe0a98163680d7c2f5ee75ebf936b55d95b98
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-23 15:47:15 +0100

    Add home_page template for home_page view

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'leads/home_page.html')
