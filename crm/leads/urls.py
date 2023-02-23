0f42fa8d27f72b06234f2d4f990eba0ac2547a9b
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-23 17:41:42 +0100

    Add lead_create view

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import lead_detail, lead_list
+from .views import lead_create, lead_detail, lead_list
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', lead_list, name='lead_list'),
-    path('<int:pk>', lead_detail, name='lead_detail'),
+    path('<int:pk>/', lead_detail, name='lead_detail'),
+    path('create/', lead_create, name='lead_create'),
 ]
