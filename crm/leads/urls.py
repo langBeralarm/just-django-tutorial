7f6aeeff4f243b52cd923ed7899305e58695863d
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-23 17:05:30 +0100

    Add lead_detail view

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import lead_list
+from .views import lead_detail, lead_list
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', lead_list, name='home_page'),
+    path('', lead_list, name='lead_list'),
+    path('<int:pk>', lead_detail, name='lead_detail'),
 ]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Lead
 
 
@@ -6,4 +6,11 @@ def lead_list(request):
     leads = Lead.objects.all()
     return render(request, 'leads/lead_list.html', {
         'leads': leads,
+    })
+
+
+def lead_detail(request, pk):
+    lead = get_object_or_404(Lead, pk=pk)
+    return render(request, 'leads/lead_detail.html', {
+        'lead': lead,
     })
