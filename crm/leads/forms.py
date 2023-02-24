79a05728eee62d7a25abd5c55e992fb1a2aae4cd
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-24 08:52:01 +0100

    Refactor lead form to inherit from ModelForm

--- crm/leads/forms.py
+++ crm/leads/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from .models import Lead
 
 
-class LeadForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    age = forms.IntegerField()
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = [
+            'first_name',
+            'last_name',
+            'age',
+            'agent',
+        ]
