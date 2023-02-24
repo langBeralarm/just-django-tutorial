b9f16e9408b8081db113b43f161883ce8eaa9a67
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-24 09:28:51 +0100

    Add lead_update view

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import lead_create, lead_detail, lead_list
+from .views import lead_create, lead_detail, lead_list, lead_update
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', lead_list, name='lead_list'),
     path('<int:pk>/', lead_detail, name='lead_detail'),
+    path('<int:pk>/update/', lead_update, name='lead_update'),
     path('create/', lead_create, name='lead_create'),
 ]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -27,3 +27,18 @@ def lead_create(request):
     return render(request, 'leads/lead_create.html', {
         'form': form,
     })
+
+
+def lead_update(request, pk):
+    lead = get_object_or_404(Lead, pk=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    return render(request, 'leads/lead_update.html', {
+        'form': form,
+        'lead': lead,
+    })
+
