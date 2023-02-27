5f16773128ed354c9af297ab13e1582db507e553
langBeralarm <leonardo.lang@beralarm.ch>
2023-02-27 10:19:39 +0100

    Refactor lead_delete view to a class based view

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView
+from .views import LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
 
 app_name = 'leads'
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', LeadListView.as_view(), name='lead_list'),
     path('<int:pk>/', LeadDetailView.as_view(), name='lead_detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead_update'),
-    path('<int:pk>/delete', lead_delete, name='lead_delete'),
+    path('<int:pk>/delete', LeadDeleteView.as_view(), name='lead_delete'),
     path('create/', LeadCreateView.as_view(), name='lead_create'),
 ]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Lead
 from .forms import LeadModelForm
-
-
-def lead_delete(request, pk):
-    lead = get_object_or_404(Lead, pk=pk)
-    lead.delete()
-    return redirect('/leads')
 
 
 class LeadListView(ListView):
@@ -34,6 +28,14 @@ class LeadUpdateView(UpdateView):
     template_name = 'leads/lead_update.html'
     queryset = Lead.objects.all()
     form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse('leads:lead_list')
+
+
+class LeadDeleteView(DeleteView):
+    template_name = 'leads/lead_delete.html'
+    queryset = Lead.objects.all()
 
     def get_success_url(self):
         return reverse('leads:lead_list')
