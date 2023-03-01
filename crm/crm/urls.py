d973c188839b6cbf87fd3ebaf49168ba59543234
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-01 09:46:13 +0100

    Add login template

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("leads/", include("leads.urls", namespace="leads")),
+    path("login/", LoginView.as_view(), name="login"),
 ]
 
 if settings.DEBUG:
