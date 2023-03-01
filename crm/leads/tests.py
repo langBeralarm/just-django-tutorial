193a2c5d150d90b79b49b4f58091213863c559f4
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-01 11:31:53 +0100

    Add test for lead views

--- crm/leads/tests.py
+++ crm/leads/tests.py
@@ -1,3 +1,42 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class LeadListTest(TestCase):
+    def test_get_request(self):
+        response = self.client.get(reverse("leads:lead_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "leads/lead_list.html")
+
+
+class LeadCreateTest(TestCase):
+    def test_get_request(self):
+        response = self.client.get(reverse("leads:lead_create"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "leads/lead_create.html")
+
+
+class LeadUpdateTest(TestCase):
+    def test_get_request(self):
+        response = self.client.get(
+            reverse(
+                "leads:lead_update",
+                kwargs={
+                    "pk": 1,
+                },
+            )
+        )
+        self.assertEqual(response.status_code, 404)
+
+
+class LeadDeleteTest(TestCase):
+    def test_get_request(self):
+        response = self.client.get(
+            reverse(
+                "leads:lead_delete",
+                kwargs={
+                    "pk": 2,
+                },
+            )
+        )
+        self.assertEqual(response.status_code, 404)
