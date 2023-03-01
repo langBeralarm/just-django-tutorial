327f923883d059fd5b576f976d9299c9ff0d14b7
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-01 15:25:56 +0100

    Add LoginRequiredMixin to lead views

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -7,6 +7,7 @@ from django.views.generic import (
     DeleteView,
 )
 from django.core.mail import send_mail
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Lead
 from .forms import LeadModelForm, CustomUserCreationForm
 
@@ -19,19 +20,19 @@ class SignUpView(CreateView):
         return reverse("login")
 
 
-class LeadListView(ListView):
+class LeadListView(LoginRequiredMixin, ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
 
 
-class LeadDetailView(DetailView):
+class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -48,7 +49,7 @@ class LeadCreateView(CreateView):
         return super(LeadCreateView, self).form_valid(form)
 
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
