2de39df4d6995e272df0c0a3ade6719d60910fbf
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-01 15:19:54 +0100

    Redirect empty path to leads path

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="leads/")),
     path("leads/", include("leads.urls", namespace="leads")),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
