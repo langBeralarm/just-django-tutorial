4f9d8f4b228db32585d9b663eeb38785cf657107
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-01 16:27:57 +0100

    Add signal to create userprofile if user was created

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Lead(models.Model):
@@ -48,3 +49,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, User)
