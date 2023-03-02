47df7b5ed1ad6447e40b35f2eeeb0e10df14d97d
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-02 15:53:25 +0100

    Update queryset to filter agents for current user

--- crm/agents/views.py
+++ crm/agents/views.py
@@ -1,20 +1,28 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
-from leads.models import Agent
+from leads.models import Agent, UserProfile
 from .forms import AgentModelForm
 
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
-    queryset = Agent.objects.all()
     context_object_name = "agents"
+
+    def get_queryset(self):
+        current_user = self.request.user
+        current_userprofile = UserProfile.objects.get(user=current_user)
+        return Agent.objects.filter(organisation=current_userprofile)
 
 
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
-    queryset = Agent.objects.all()
     context_object_name = "agent"
+
+    def get_queryset(self):
+        current_user = self.request.user
+        current_userprofile = UserProfile.objects.get(user=current_user)
+        return Agent.objects.filter(organisation=current_userprofile)
 
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
@@ -33,16 +41,24 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 
 class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = "agents/agent_delete.html"
-    queryset = Agent.objects.all()
 
     def get_success_url(self):
         return reverse("agents:agent_list")
+
+    def get_queryset(self):
+        current_user = self.request.user
+        current_userprofile = UserProfile.objects.get(user=current_user)
+        return Agent.objects.filter(organisation=current_userprofile)
 
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "agents/agent_update.html"
-    queryset = Agent.objects.all()
     form_class = AgentModelForm
 
     def get_success_url(self):
         return reverse("agents:agent_list")
+
+    def get_queryset(self):
+        current_user = self.request.user
+        current_userprofile = UserProfile.objects.get(user=current_user)
+        return Agent.objects.filter(organisation=current_userprofile)
