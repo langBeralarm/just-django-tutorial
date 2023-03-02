b80424e12870bf150f33aaa38fb34aed41417efa
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-02 13:43:24 +0100

    Add agent list view

--- crm/agents/views.py
+++ crm/agents/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from leads.models import Agent
 
-# Create your views here.
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+    queryset = Agent.objects.all()
+    context_object_name = "agents"
