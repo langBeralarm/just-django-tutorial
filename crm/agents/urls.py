f0fc4d5cb38451ff4000541ddce0370394ef74a4
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-02 14:27:58 +0100

    Add agent create view

--- crm/agents/urls.py
+++ crm/agents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AgentListView, AgentDetailView
+from .views import AgentListView, AgentDetailView, AgentCreateView
 
 app_name = "agents"
 
 urlpatterns = [
     path("", AgentListView.as_view(), name="agent_list"),
     path("<int:pk>", AgentDetailView.as_view(), name="agent_detail"),
+    path("create/", AgentCreateView.as_view(), name="agent_create"),
 ]
--- crm/agents/views.py
+++ crm/agents/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import reverse
 from leads.models import Agent
+from .forms import AgentModelForm
 
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
@@ -13,3 +15,17 @@ class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
     queryset = Agent.objects.all()
     context_object_name = "agent"
+
+
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = "agents/agent_create.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse("agents:agent_list")
+
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organisation = self.request.user
+        agent.save()
+        super(AgentModelForm, self).form_valid(form)
